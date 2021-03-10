761b06a610ac61bf245ab6b02eff4a3348385661
thevinter <theniki98@gmail.com>
2021-03-10 13:24:01 +0100

    links

---
+++ src/Link.java
@@ -0,0 +1,17 @@
+public class Link {
+    private Node to;
+    private Node from;
+    private int weight;
+
+    public Link(Node to, Node from){
+        this.to = to;
+        this.from = from;
+        this.weight = 0;
+    }
+
+    public Link(Node to, Node from, int weight){
+        this.to = to;
+        this.from = from;
+        this.weight = weight
+    }
+}
