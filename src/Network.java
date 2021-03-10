d57e53ff6a2cc6c6056bda8b1e8bed3a3dfd93e8
thevinter <theniki98@gmail.com>
2021-03-10 13:06:39 +0100

    documentation

--- src/Network.java
+++ src/Network.java
@@ -8,11 +8,22 @@ public class Network {
         return true;
     }
 
+    /**
+     * This allows to add a Location the network if the user already has a Location object and a list of incoming and outgoing Transitions
+     * @param l The Location object
+     * @param from The incoming Transition nodes
+     * @param to The outgoing Transition nodes
+     */
     public void addLocation(Location l, ArrayList<Transition> from, ArrayList<Transition> to){
         l.addLinks(from, to);
         nodes.add(l);
     }
 
+    /**
+     * This allows to add a Location to the Network without having the Transition and using integer IDs for the connections
+     * @param from The list of incoming Transition IDs
+     * @param to The list of outgoing Transition IDs
+     */
     public void addLocation(ArrayList<Integer> from, ArrayList<Integer> to){
         Location l = new Location();
         for(Integer i : from){
@@ -48,11 +59,22 @@ public class Network {
         }
     }
 
+    /**
+     * This adds a transition to the Network if we have everything we need
+     * @param t The transition we want to add
+     * @param from The incoming Locations
+     * @param to The outgoing locations
+     */
     public void addTransition(Transition t, ArrayList<Location> from, ArrayList<Location> to){
         t.addLinks(from, to);
         nodes.add(t);
     }
 
+    /**
+     * This allows to add a Transition to the Network without having the Transition and using integer IDs for the connections
+     * @param from The list of IDs of the incoming nodes
+     * @param to  The list of IDs of the outgoing nodes
+     */
     public void addTransition(ArrayList<Integer> from, ArrayList<Integer> to){
         Transition t = new Transition();
         for(Integer i : from){
@@ -87,18 +109,31 @@ public class Network {
         }
     }
 
+    /**
+     * This creates a new empty transition if the user wants to
+     * @return A new transition
+     */
     public Transition createTransition(){
         System.out.println("The following transition doesn't exist. Do you want to create one?");
         //TODO: Implement scanner and choice
         return new Transition();
     }
 
+    /**
+     * This creates a new empty Location if the user wants to
+     * @return A new location
+     */
     public Location createLocation(){
         System.out.println("The following location doesn't exist. Do you want to create one?");
         //TODO: Implement scanner and choice
         return new Location();
     }
 
+    /**
+     * Finds a node in the Network given an id
+     * @param id The id of the node we want to search
+     * @return The node requested, null if the node is not present
+     */
     public Node findNode(int id){
         for(Node n: nodes){
             if(n.getId() == id) return n;
