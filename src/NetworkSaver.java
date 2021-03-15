a07bd7140a1dfaa2231cf4e50098a072316631cc
xJau <bignotti.maurizio97@gmail.com>
2021-03-15 11:00:02 +0100

    NetworkSaver updated and working

--- src/NetworkSaver.java
+++ src/NetworkSaver.java
@@ -5,23 +5,32 @@ import com.fasterxml.jackson.core.util.DefaultPrettyPrinter;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.List;
 
 public class NetworkSaver {
 
-    private String activeDocument = "files/default";
-    private ArrayList<Network> networks;
+
+
+    private String activeDocument = "default";
+    private List<Network> networks;
     private Network activeNet;
-    private ArrayList<Location> locations;
-    private ArrayList<Transition> transitions;
+    private List<Location>  locations = new ArrayList<>();
+    private List<Transition> transitions = new ArrayList<>();
 
+    public NetworkSaver(ArrayList<Network> nets){
+        networks = nets;
+    }
 
-    public void activeNetwork(Network n) {
-        this.activeNet = n;
-        this.locations = n.getLocations();
-        this.transitions = n.getTransitions();
+    //TODO v5 Constructor for change name of activeDocument
+
+    public void activeNet(Network n) {
+        activeNet = n;
+        locations = n.getLocations();
+        transitions = n.getTransitions();
 
     }
-    public void createDocument() {
+
+    public  void createDocument() {
 
         try {
             JsonGenerator jsonGenerator = new JsonFactory().createGenerator(new FileOutputStream(activeDocument+".json"));
@@ -31,11 +40,14 @@ public class NetworkSaver {
             jsonGenerator.writeStartObject(); //start Net Space
             jsonGenerator.writeArrayFieldStart("Networks");
             for(Network n: networks) { //start Nets array
-                jsonGenerator.writeObjectFieldStart(n.getName()); //start Net
+                activeNet(n);
+                jsonGenerator.writeStartObject(); //start Net
+                jsonGenerator.writeStringField("name",n.getName()); //name net field
 
                 jsonGenerator.writeArrayFieldStart("locations"); //start location array
                 for(Location l: locations) {
-                    jsonGenerator.writeObjectFieldStart(String.valueOf(l.getId()));//start location
+                    jsonGenerator.writeStartObject();//start location
+                    jsonGenerator.writeNumberField("id",l.getId());//id location field
                     //TODO v2 tokens field
                     jsonGenerator.writeEndObject();//end location
                 }
@@ -43,22 +55,27 @@ public class NetworkSaver {
 
                 jsonGenerator.writeArrayFieldStart("transitions"); //start transitions array
                 for(Transition t: transitions) {
-                    jsonGenerator.writeObjectFieldStart(String.valueOf(t.getId()));//start transition
+                    jsonGenerator.writeStartObject();//start transition
+                    jsonGenerator.writeNumberField("id",t.getId()); //id transition field
                     //TODO v4 priority field
-                    jsonGenerator.writeArrayFieldStart("in");//start ingoing nodes
+                    jsonGenerator.writeArrayFieldStart("in");//start ingoing nodes array
                     for(Link l: t.links) {
                         if(l.getDestination().getId() == t.getId()) {
-                            jsonGenerator.writeNumberField("node",l.getFrom().getId());
+                            jsonGenerator.writeStartObject();//start ingoing node
+                            jsonGenerator.writeNumberField("node",l.getFrom().getId());//id ingoing node field
                             //TODO v2 weight field
+                            jsonGenerator.writeEndObject();//end ingoing node
                         }
                     }
-                    jsonGenerator.writeEndArray();//end ingoing nodes
+                    jsonGenerator.writeEndArray();//end ingoing nodes array
 
-                    jsonGenerator.writeArrayFieldStart("out");//start outgoing nodes
+                    jsonGenerator.writeArrayFieldStart("out");//start outgoing nodes array
                     for(Link l: t.links) {
                         if(l.getFrom().getId() == t.getId()) {
-                            jsonGenerator.writeNumberField("node",l.getDestination().getId());
+                            jsonGenerator.writeStartObject();//start outgoing node
+                            jsonGenerator.writeNumberField("node",l.getDestination().getId());//id outgoing node field
                             //TODO v2 weight field
+                            jsonGenerator.writeEndObject();//end outgoing node array
                         }
                     }
                     jsonGenerator.writeEndArray();//end outgoing nodes
@@ -69,7 +86,8 @@ public class NetworkSaver {
 
                 jsonGenerator.writeEndObject();// end Net
 
-            } //end Nets array
+            }
+            jsonGenerator.writeEndArray(); //end Nets array
             jsonGenerator.writeEndObject(); //end Net Space
 
             jsonGenerator.flush();
